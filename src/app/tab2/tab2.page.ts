1a53e24cafce5fa5fe53f0f4a9684a45f2f4e7f0
ionicArabic <ionicarabic@gmail.com>
2021-06-06 00:23:09 +0300

    extra varifications

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,10 +9,18 @@ export class Tab2Page {
   name: string ;
   birthYear: number;
   age: number;
+  rongDate: string;
 
   constructor() {}
 
   calcAge() {
+    if(this.birthYear < 1920 || this.birthYear > 2021 ){
+      this.rongDate = "ادخل التاريخ الميلادي";
+      this.birthYear = undefined;
+      this.age = undefined;
+      return (console.log("ادخل التاريخ الميلادي"));
+    } else
+    this.rongDate = undefined;
     this.age = 2021 - this.birthYear;
   }
 
